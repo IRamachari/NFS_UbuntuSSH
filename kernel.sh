dd3143dc1cbe17716e766fab9e3a701dc976ecdb
Finix-Projetcs <jarbull87@gmail.com>
2021-11-03 13:29:30 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,3 +1,3 @@
-git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/Finix_kernel-4.9-rosy -b eleven && cd Finix_kernel-4.9-rosy
+git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/Finix_kernel-4.9-rosy -b 11 && cd Finix_kernel-4.9-rosy
 git config --global user.email jarbull87@gmail.com
 git config --global user.name AnGgIt88
