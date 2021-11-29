7610ffc4c5363bbc562487144551659099a8eacc
Finix-Projetcs <jarbull87@gmail.com>
2021-11-29 11:42:48 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,4 @@
 git config --global user.email jarbull87@gmail.com
 git config --global user.name AnGgIt88
-git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/kernel_xiaomi_rosy -b eleven && cd kernel_xiaomi_rosy
+git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/kernel_xiaomi_rosy -b eleven kernel
 echo done..
