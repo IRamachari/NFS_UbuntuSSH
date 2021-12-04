f5f9fa8ebb6bba8c6d361f966bc8136239daab6e
Finix-Projetcs <jarbull87@gmail.com>
2021-12-04 18:06:03 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,6 @@
 git config --global user.email jarbull87@gmail.com
 git config --global user.name AnGgIt88
 git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/kernel_xiaomi_rosy -b eleven kernel
+cd kernel
+git fetch https://github.com/ElectroPerf/android_kernel_asus_sdm660-LTO master
 echo done..
