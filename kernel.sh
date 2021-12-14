0777258570ebc636a5c40a6275c042c66cbccbfb
FinixDev™ <jarbull87@gmail.com>
2021-12-14 08:45:29 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,4 @@
-git config --global user.email jarbull87@gmail.com
-git config --global user.name AnGgIt88
-git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/kernel_xiaomi_rosy -b eleven kernel
+git config --global user.email jarbull86@gmail.com
+git config --global user.name NFS86
+git clone https://AnGgIt88:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b eleven kernel
 echo done..
