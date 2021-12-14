ac0076eab2a335c001acfae62190454c90c8963d
FinixDev™ <jarbull87@gmail.com>
2021-12-14 14:38:19 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,13 @@
 git config --global user.email jarbull86@gmail.com
 git config --global user.name NFS86
-git clone https://AnGgIt88:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b eleven kernel
+git clone https://AnGgIt88:$GH_TOKEN@github.com/NFS-projects/gcc-arm
+cd gcc-arm
+git reset --hard d3f70d2d2e53ba78650f8679494e0095b6e2b79c
+git push -f origin HEAD
+cd ../
+git clone https://AnGgIt88:$GH_TOKEN@github.com/NFS-projects/gcc-arm64
+cd gcc-arm64
+git reset --hard 8dec784f8d21a71eb2b1dcf49c7271f3c25c198d
+git push -f origin HEAD
 echo done..
+exit
