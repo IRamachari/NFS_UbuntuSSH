4fb422bb8e1da0200b123f964368d857d52172e7
FinixDev™ <jarbull87@gmail.com>
2021-12-16 08:06:10 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,13 +1,13 @@
-git config --global user.email jarbull86@gmail.com
-git config --global user.name NFS86
+git config --global user.email jarbull87@gmail.com
+git config --global user.name AnGgIt88
 git clone https://AnGgIt88:$GH_TOKEN@github.com/NFS-projects/gcc-arm
 cd gcc-arm
-git reset --hard b6b79b7bb988793e02fd94d9c752eb4bc09a1608
-git push -f origin HEAD
+git checkout -b 12.x
+git push --set-upstream origin 12.x
 cd ../
 git clone https://AnGgIt88:$GH_TOKEN@github.com/NFS-projects/gcc-arm64
 cd gcc-arm64
-git reset --hard bac0e1241ab5a9adab2971559eb6cf1a8656bb8c
-git push -f origin HEAD
+git checkout -b 12.x
+git push --set-upstream origin 12.x
 echo done..
 exit
