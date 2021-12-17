e6e174b883c2c2d2d8ea88c77773a6831b4999ce
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-17 19:35:52 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,7 +1,4 @@
 git config --global user.email jarbull86@gmail.com
 git config --global user.name NFS86
-git clone https://NFS86:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b eleven
-cd kernel_xiaomi_rosy
-git checkout -b exp
-git push --set-upstream origin exp
+git clone https://NFS86:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b exp kernel
 echo done..
