288350632fbf519727b135937d2798dffb7facd8
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-18 23:07:56 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,6 @@
 git config --global user.email jarbull86@gmail.com
 git config --global user.name NFS86
-git clone https://NFS86:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b exp kernel
+git clone https://NFS86:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b eleven kernel
 cd kernel
-git fetch https://github.com/GhostMaster69-dev/android_kernel_xiaomi_vince stable
+git fetch https://github.com/KudProject/kernel_msm-4.9 a11/vdso32
 echo done..
