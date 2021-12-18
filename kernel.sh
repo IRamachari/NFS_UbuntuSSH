74839263de93f7588263f6f27bc71b651008fc5f
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-18 22:27:32 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,6 @@
 git config --global user.email jarbull86@gmail.com
 git config --global user.name NFS86
-git clone https://NFS86:$GH_TOKEN@github.com/NFS86/system_sepolicy -b spark
-cd system_sepolicy
-git fetch https://github.com/LineageOS/android_system_sepolicy
+git clone https://NFS86:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b exp kernel
+cd kernel
+git fetch https://github.com/GhostMaster69-dev/android_kernel_xiaomi_vince stable
 echo done..
