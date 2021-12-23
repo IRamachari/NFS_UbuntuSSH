10cd357b78b368792a971b6bc7772ee9b140c9da
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-24 05:31:21 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,5 +1,5 @@
 git config --global user.email jarbull86@gmail.com
 git config --global user.name NFS86
-git clone https://NFS86:$GH_TOKEN@github.com/NFS86/bionic -b spark bionic
-cd bionic
-git fetch https://github.com/PixelExperience/bionic
+git clone https://NFS86:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b twelve
+cd kernel_xiaomi_rosy
+git fetch https://github.com/NFS-projects/kernel_xiaomi_rosy eleven
