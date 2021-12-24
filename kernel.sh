99fadd12d3c63783b816172dccabcc982bc1fee9
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-24 22:56:08 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,5 +1,5 @@
 git config --global user.email jarbull86@gmail.com
 git config --global user.name NFS86
-git clone https://github.com/Octavi-OS/platform_vendor_octavi -b 12
-cd platform_vendor_octavi
-git fetch https://github.com/AnGgIt88/platform_vendor_spark spark
+git clone https://github.com/Octavi-OS/platform_system_sepolicy -b 12
+cd platform_system_sepolicy
+git fetch https://github.com/NFS86/system_sepolicy spark
