896f3397f12d4a3f91d30eb639415fa5a2c0cacf
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-24 22:39:07 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,5 +1,5 @@
 git config --global user.email jarbull86@gmail.com
 git config --global user.name NFS86
-git clone https://NFS86:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b twelve
-cd kernel_xiaomi_rosy
-git fetch https://github.com/NFS-projects/kernel_xiaomi_rosy eleven
+git clone https://github.com/Octavi-OS/platform_vendor_octavi -b 12
+cd platform_vendor_octavi
+git fetch https://github.com/AnGgIt88/platform_vendor_spark spark
