ee4175b14c22cc7cd19349ccab0096ab351615d8
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-26 15:03:24 +0700

    Kernel gas

--- kernel.sh
+++ kernel.sh
@@ -1,5 +1,3 @@
 git config --global user.email jarbull86@gmail.com
 git config --global user.name NFS86
-git clone https://github.com/Octavi-OS/platform_system_sepolicy -b 12
-cd platform_system_sepolicy
-git fetch https://github.com/NFS86/system_sepolicy spark
+git clone https://NFS86:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b eleven
