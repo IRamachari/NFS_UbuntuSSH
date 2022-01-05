a92dea434019d26c1d0c3afb78b778c7031338e8
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-01-06 00:06:22 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,3 +1,3 @@
 git config --global user.email jarbull86@gmail.com
 git config --global user.name NFS86
-git clone https://NFS86:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b eleven
+git clone https://NFS86:$GH_TOKEN@github.com/NFS-projects/kernel_xiaomi_rosy -b twelve
